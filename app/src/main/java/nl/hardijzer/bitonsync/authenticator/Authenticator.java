6804527650ffe16c926205f238e556b5d2128702
Frans-Willem Hardijzer <fw@hardijzer.nl>
2016-05-06 17:49:33 +0200

    Added GPL 3 license.

--- app/src/main/java/nl/hardijzer/bitonsync/authenticator/Authenticator.java
+++ app/src/main/java/nl/hardijzer/bitonsync/authenticator/Authenticator.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2010-2016 Frans-Willem Hardijzer
+ *
+ * This file is part of BitonSync.
+ *
+ * BitonSync is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BitonSync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BitonSync.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package nl.hardijzer.bitonsync.authenticator;
 
 import nl.hardijzer.bitonsync.Constants;
